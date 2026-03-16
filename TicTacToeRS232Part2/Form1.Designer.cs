dddb09cbe88fe86d62e4eb462b53c634a459195a
Mateusz Dziadek <mateusz.dziadek03@gmail.com>
2026-03-16 21:52:36 +0100

    Added receiver project files

---
+++ TicTacToeRS232Part2/Form1.Designer.cs
@@ -0,0 +1,337 @@
+﻿namespace TicTacToeRS232Part2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serialPort1 = new System.IO.Ports.SerialPort();
+            this.button3_3 = new System.Windows.Forms.Button();
+            this.button3_2 = new System.Windows.Forms.Button();
+            this.button3_1 = new System.Windows.Forms.Button();
+            this.button2_3 = new System.Windows.Forms.Button();
+            this.button2_2 = new System.Windows.Forms.Button();
+            this.button2_1 = new System.Windows.Forms.Button();
+            this.button1_3 = new System.Windows.Forms.Button();
+            this.button1_2 = new System.Windows.Forms.Button();
+            this.button1_1 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.labelStopBits = new System.Windows.Forms.Label();
+            this.comboBoxStopBits = new System.Windows.Forms.ComboBox();
+            this.labelHandshake = new System.Windows.Forms.Label();
+            this.labelBoxParity = new System.Windows.Forms.Label();
+            this.labelDataBits = new System.Windows.Forms.Label();
+            this.comboBoxHandshake = new System.Windows.Forms.ComboBox();
+            this.comboBoxParity = new System.Windows.Forms.ComboBox();
+            this.comboBoxDataBits = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.labelBaudRate = new System.Windows.Forms.Label();
+            this.labelPort = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button3_3
+            // 
+            this.button3_3.Location = new System.Drawing.Point(479, 272);
+            this.button3_3.Name = "button3_3";
+            this.button3_3.Size = new System.Drawing.Size(50, 50);
+            this.button3_3.TabIndex = 77;
+            this.button3_3.UseVisualStyleBackColor = true;
+            // 
+            // button3_2
+            // 
+            this.button3_2.Location = new System.Drawing.Point(420, 272);
+            this.button3_2.Name = "button3_2";
+            this.button3_2.Size = new System.Drawing.Size(50, 50);
+            this.button3_2.TabIndex = 76;
+            this.button3_2.UseVisualStyleBackColor = true;
+            // 
+            // button3_1
+            // 
+            this.button3_1.Location = new System.Drawing.Point(363, 272);
+            this.button3_1.Name = "button3_1";
+            this.button3_1.Size = new System.Drawing.Size(50, 50);
+            this.button3_1.TabIndex = 75;
+            this.button3_1.UseVisualStyleBackColor = true;
+            // 
+            // button2_3
+            // 
+            this.button2_3.Location = new System.Drawing.Point(479, 216);
+            this.button2_3.Name = "button2_3";
+            this.button2_3.Size = new System.Drawing.Size(50, 50);
+            this.button2_3.TabIndex = 74;
+            this.button2_3.UseVisualStyleBackColor = true;
+            // 
+            // button2_2
+            // 
+            this.button2_2.Location = new System.Drawing.Point(420, 216);
+            this.button2_2.Name = "button2_2";
+            this.button2_2.Size = new System.Drawing.Size(50, 50);
+            this.button2_2.TabIndex = 73;
+            this.button2_2.UseVisualStyleBackColor = true;
+            // 
+            // button2_1
+            // 
+            this.button2_1.Location = new System.Drawing.Point(363, 216);
+            this.button2_1.Name = "button2_1";
+            this.button2_1.Size = new System.Drawing.Size(50, 50);
+            this.button2_1.TabIndex = 72;
+            this.button2_1.UseVisualStyleBackColor = true;
+            // 
+            // button1_3
+            // 
+            this.button1_3.Location = new System.Drawing.Point(479, 160);
+            this.button1_3.Name = "button1_3";
+            this.button1_3.Size = new System.Drawing.Size(50, 50);
+            this.button1_3.TabIndex = 71;
+            this.button1_3.UseVisualStyleBackColor = true;
+            // 
+            // button1_2
+            // 
+            this.button1_2.Location = new System.Drawing.Point(420, 160);
+            this.button1_2.Name = "button1_2";
+            this.button1_2.Size = new System.Drawing.Size(50, 50);
+            this.button1_2.TabIndex = 70;
+            this.button1_2.UseVisualStyleBackColor = true;
+            // 
+            // button1_1
+            // 
+            this.button1_1.Location = new System.Drawing.Point(363, 160);
+            this.button1_1.Name = "button1_1";
+            this.button1_1.Size = new System.Drawing.Size(50, 50);
+            this.button1_1.TabIndex = 69;
+            this.button1_1.UseVisualStyleBackColor = true;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(364, 54);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(180, 56);
+            this.trackBar1.TabIndex = 68;
+            // 
+            // labelStopBits
+            // 
+            this.labelStopBits.AutoSize = true;
+            this.labelStopBits.Location = new System.Drawing.Point(21, 199);
+            this.labelStopBits.Name = "labelStopBits";
+            this.labelStopBits.Size = new System.Drawing.Size(67, 16);
+            this.labelStopBits.TabIndex = 67;
+            this.labelStopBits.Text = "BoxParity:";
+            // 
+            // comboBoxStopBits
+            // 
+            this.comboBoxStopBits.FormattingEnabled = true;
+            this.comboBoxStopBits.Location = new System.Drawing.Point(115, 196);
+            this.comboBoxStopBits.Name = "comboBoxStopBits";
+            this.comboBoxStopBits.Size = new System.Drawing.Size(217, 24);
+            this.comboBoxStopBits.TabIndex = 66;
+            // 
+            // labelHandshake
+            // 
+            this.labelHandshake.AutoSize = true;
+            this.labelHandshake.Location = new System.Drawing.Point(18, 230);
+            this.labelHandshake.Name = "labelHandshake";
+            this.labelHandshake.Size = new System.Drawing.Size(80, 16);
+            this.labelHandshake.TabIndex = 65;
+            this.labelHandshake.Text = "Handshake:";
+            // 
+            // labelBoxParity
+            // 
+            this.labelBoxParity.AutoSize = true;
+            this.labelBoxParity.Location = new System.Drawing.Point(21, 166);
+            this.labelBoxParity.Name = "labelBoxParity";
+            this.labelBoxParity.Size = new System.Drawing.Size(67, 16);
+            this.labelBoxParity.TabIndex = 64;
+            this.labelBoxParity.Text = "BoxParity:";
+            // 
+            // labelDataBits
+            // 
+            this.labelDataBits.AutoSize = true;
+            this.labelDataBits.Location = new System.Drawing.Point(21, 129);
+            this.labelDataBits.Name = "labelDataBits";
+            this.labelDataBits.Size = new System.Drawing.Size(61, 16);
+            this.labelDataBits.TabIndex = 63;
+            this.labelDataBits.Text = "DataBits:";
+            // 
+            // comboBoxHandshake
+            // 
+            this.comboBoxHandshake.FormattingEnabled = true;
+            this.comboBoxHandshake.Location = new System.Drawing.Point(115, 230);
+            this.comboBoxHandshake.Name = "comboBoxHandshake";
+            this.comboBoxHandshake.Size = new System.Drawing.Size(217, 24);
+            this.comboBoxHandshake.TabIndex = 62;
+            // 
+            // comboBoxParity
+            // 
+            this.comboBoxParity.FormattingEnabled = true;
+            this.comboBoxParity.Location = new System.Drawing.Point(115, 166);
+            this.comboBoxParity.Name = "comboBoxParity";
+            this.comboBoxParity.Size = new System.Drawing.Size(217, 24);
+            this.comboBoxParity.TabIndex = 61;
+            // 
+            // comboBoxDataBits
+            // 
+            this.comboBoxDataBits.FormattingEnabled = true;
+            this.comboBoxDataBits.Location = new System.Drawing.Point(115, 126);
+            this.comboBoxDataBits.Name = "comboBoxDataBits";
+            this.comboBoxDataBits.Size = new System.Drawing.Size(217, 24);
+            this.comboBoxDataBits.TabIndex = 60;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(115, 307);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(217, 22);
+            this.textBox1.TabIndex = 59;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(115, 260);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(217, 30);
+            this.button2.TabIndex = 58;
+            this.button2.Text = "Open";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // labelBaudRate
+            // 
+            this.labelBaudRate.AutoSize = true;
+            this.labelBaudRate.Location = new System.Drawing.Point(21, 86);
+            this.labelBaudRate.Name = "labelBaudRate";
+            this.labelBaudRate.Size = new System.Drawing.Size(71, 16);
+            this.labelBaudRate.TabIndex = 57;
+            this.labelBaudRate.Text = "BaudRate:";
+            // 
+            // labelPort
+            // 
+            this.labelPort.AutoSize = true;
+            this.labelPort.Location = new System.Drawing.Point(21, 50);
+            this.labelPort.Name = "labelPort";
+            this.labelPort.Size = new System.Drawing.Size(34, 16);
+            this.labelPort.TabIndex = 56;
+            this.labelPort.Text = "Port:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(115, 86);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(217, 24);
+            this.comboBox2.TabIndex = 55;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(115, 47);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 54;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(257, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 30);
+            this.button1.TabIndex = 53;
+            this.button1.Text = "Refresh";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 373);
+            this.Controls.Add(this.button3_3);
+            this.Controls.Add(this.button3_2);
+            this.Controls.Add(this.button3_1);
+            this.Controls.Add(this.button2_3);
+            this.Controls.Add(this.button2_2);
+            this.Controls.Add(this.button2_1);
+            this.Controls.Add(this.button1_3);
+            this.Controls.Add(this.button1_2);
+            this.Controls.Add(this.button1_1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.labelStopBits);
+            this.Controls.Add(this.comboBoxStopBits);
+            this.Controls.Add(this.labelHandshake);
+            this.Controls.Add(this.labelBoxParity);
+            this.Controls.Add(this.labelDataBits);
+            this.Controls.Add(this.comboBoxHandshake);
+            this.Controls.Add(this.comboBoxParity);
+            this.Controls.Add(this.comboBoxDataBits);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.labelBaudRate);
+            this.Controls.Add(this.labelPort);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "RS232 - receiver";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button3_3;
+        private System.Windows.Forms.Button button3_2;
+        private System.Windows.Forms.Button button3_1;
+        private System.Windows.Forms.Button button2_3;
+        private System.Windows.Forms.Button button2_2;
+        private System.Windows.Forms.Button button2_1;
+        private System.Windows.Forms.Button button1_3;
+        private System.Windows.Forms.Button button1_2;
+        private System.Windows.Forms.Button button1_1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label labelStopBits;
+        private System.Windows.Forms.ComboBox comboBoxStopBits;
+        private System.Windows.Forms.Label labelHandshake;
+        private System.Windows.Forms.Label labelBoxParity;
+        private System.Windows.Forms.Label labelDataBits;
+        private System.Windows.Forms.ComboBox comboBoxHandshake;
+        private System.Windows.Forms.ComboBox comboBoxParity;
+        private System.Windows.Forms.ComboBox comboBoxDataBits;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label labelBaudRate;
+        private System.Windows.Forms.Label labelPort;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ TicTacToeRS232Part2/Form1.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace TicTacToeRS232Part2
+{
+    public partial class Form1 : Form
+    {
+        // A delegate used to safely call a method in a GUI thread
+        delegate void SetTextCallback(string text);
+        string InputData = String.Empty;
+        private System.Windows.Forms.Button[,] TTTbuttons;
+        private bool CheckWin = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(port_DataReceived_1);
+            Board(); // Initializing the game board
+        }
+
+        // Handling data reception from a serial port
+        private void port_DataReceived_1(object sender, SerialDataReceivedEventArgs e)
+        {
+            InputData = serialPort1.ReadExisting(); // read data
+            if (InputData != String.Empty)
+            {
+                // Call the SetText method 
+                this.BeginInvoke(new SetTextCallback(SetText), new object[] { InputData });
+            }
+        }
+
+        // Initializing the game board
+        private void Board()
+        {
+            TTTbuttons = new System.Windows.Forms.Button[3, 3]
+            {
+                { button1_1, button1_2, button1_3 },
+                { button2_1, button2_2, button2_3 },
+                { button3_1, button3_2, button3_3 }
+            };
+
+            // Handling click events
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    TTTbuttons[i, j].Click += TTTbuttons_Click;
+                }
+            }
+        }
+
+        // Click the game button (Player O's move)
+        private void TTTbuttons_Click(object sender, EventArgs e)
+        {
+            if (CheckWin)
+                return;
+
+            System.Windows.Forms.Button btn = sender as System.Windows.Forms.Button;
+            if (btn != null && string.IsNullOrEmpty(btn.Text) && serialPort1.IsOpen)
+            {
+                string symbol = "O";
+                btn.Text = symbol;
+                CheckWinCondition();
+                serialPort1.Write("TTT:" + btn.Name + ":" + symbol);
+            }
+        }
+
+        private void CheckWinCondition()
+        {
+            string[,] board = new string[3, 3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    board[i, j] = TTTbuttons[i, j].Text;
+                }
+            }
+
+            // Line checking
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[i, 0] != "" &&
+                    board[i, 0] == board[i, 1] &&
+                    board[i, 1] == board[i, 2])
+                {
+                    GameEnd(board[i, 0]);
+                    return;
+                }
+            }
+
+            // Column checking
+            for (int j = 0; j < 3; j++)
+            {
+                if (board[0, j] != "" &&
+                    board[0, j] == board[1, j] &&
+                    board[1, j] == board[2, j])
+                {
+                    GameEnd(board[0, j]);
+                    return;
+                }
+            }
+
+            // Diagonal 1
+            if (board[0, 0] != "" &&
+                board[0, 0] == board[1, 1] &&
+                board[1, 1] == board[2, 2])
+            {
+                GameEnd(board[0, 0]);
+                return;
+            }
+
+            // Diagonal 2
+            if (board[0, 2] != "" &&
+                board[0, 2] == board[1, 1] &&
+                board[1, 1] == board[2, 0])
+            {
+                GameEnd(board[0, 2]);
+                return;
+            }
+        }
+
+        private void GameEnd(string zwyciezca)
+        {
+            CheckWin = true;
+
+            foreach (System.Windows.Forms.Button b in TTTbuttons)
+            {
+                b.Enabled = false;
+            }
+
+            MessageBox.Show("Player won: " + zwyciezca);
+        }
+
+        // Displaying received data and handling messages
+        private void SetText(string text)
+        {
+            // Responses to simple text messages
+            if (text == "BTN1")
+                this.textBox1.Text = "Button 1 was pressed";
+            else if (text == "BTN2")
+                this.textBox1.Text = "Button 2 was pressed";
+            else if (text == "BTN3")
+                this.textBox1.Text = "Button 3 was pressed";
+            else if (text.StartsWith("TRACKBAR:"))
+            {
+                // Reading trackbar values from data
+                string valueStr = text.Substring(9);
+                int valueT;
+                if (int.TryParse(valueStr, out valueT))
+                {
+                    trackBar1.Value = valueT;
+                }
+                this.textBox1.Text = "TrackBar: " + valueStr;
+            }
+            else
+                this.textBox1.Text = text;
+
+            // Handling the Tic-Tac-Toe game message
+            if (text.StartsWith("TTT:"))
+            {
+                string[] parts = text.Split(':');
+                if (parts.Length == 3)
+                {
+                    string buttonName = parts[1];
+                    string symbol = parts[2];
+                    foreach (System.Windows.Forms.Button b in TTTbuttons)
+                    {
+                        if (b.Name == buttonName && string.IsNullOrEmpty(b.Text))
+                        {
+                            b.Text = symbol;
+                            CheckWinCondition();
+                            break;
+                        }
+                    }
+                }
+                return;
+            }
+
+            this.textBox1.Text = text;
+
+            // If a number is received, move the slider
+            if (int.TryParse(text.Trim(), out int value))
+            {
+                if (value >= trackBar1.Minimum && value <= trackBar1.Maximum)
+                {
+                    trackBar1.Value = value;
+                }
+            }
+        }
+
+        // Refresh the list of COM ports
+        private void button1_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+            string[] ports = SerialPort.GetPortNames();
+            Array.Sort(ports);
+
+            if (ports.Length > 0)
+            {
+                foreach (string port in ports)
+                {
+                    comboBox1.Items.Add(port);
+                }
+
+                comboBox1.SelectedIndex = 0;
+            }
+            else
+            {
+                MessageBox.Show("No COM ports were found.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        // Opening/closing the serial port
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.Text == "Open")
+            {
+                // Port settings
+                serialPort1.PortName = comboBox1.Text;
+                serialPort1.BaudRate = Convert.ToInt32(comboBox2.Text);
+                serialPort1.DataBits = Convert.ToInt16(comboBoxDataBits.Text);
+                serialPort1.Parity = (Parity)Enum.Parse(typeof(Parity), comboBoxParity.Text);
+                serialPort1.StopBits = (StopBits)Enum.Parse(typeof(StopBits), comboBoxStopBits.Text);
+                serialPort1.Handshake = (Handshake)Enum.Parse(typeof(Handshake), comboBoxHandshake.Text);
+
+                try
+                {
+                    serialPort1.Open();
+                    button2.Text = "Close";
+                }
+                catch (UnauthorizedAccessException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else if (button2.Text == "Close")
+            {
+                try
+                {
+                    serialPort1.Close();
+                    button2.Text = "Open";
+                }
+                catch (UnauthorizedAccessException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        // Form startup settings (initialization of the list of ports and parameters)
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // List of standard baud rates
+            comboBox2.Items.Add(300);
+            comboBox2.Items.Add(600);
+            comboBox2.Items.Add(1200);
+            comboBox2.Items.Add(2400);
+            comboBox2.Items.Add(9600);
+            comboBox2.Items.Add(14400);
+            comboBox2.Items.Add(19200);
+            comboBox2.Items.Add(38400);
+            comboBox2.Items.Add(57600);
+            comboBox2.Items.Add(115200);
+            comboBox2.Items.ToString();
+            comboBox2.Text = comboBox2.Items[0].ToString();
+
+            // Number of data bits
+            comboBoxDataBits.Items.Add(5);
+            comboBoxDataBits.Items.Add(6);
+            comboBoxDataBits.Items.Add(7);
+            comboBoxDataBits.Items.Add(8);
+            comboBoxDataBits.Text = "8"; // default
+
+            // Parity
+            comboBoxParity.Items.AddRange(Enum.GetNames(typeof(Parity)));
+            comboBoxParity.Text = "None"; // default
+
+            // Number of stop bits
+            comboBoxStopBits.Items.AddRange(Enum.GetNames(typeof(StopBits)));
+            comboBoxStopBits.Text = "One"; // default
+
+            // Handshake constrol
+            comboBoxHandshake.Items.AddRange(Enum.GetNames(typeof(Handshake)));
+            comboBoxHandshake.Text = "None"; // default
+
+            // Detecting available COM ports
+            string[] ArrayComPortsNames = null;
+            int index = -1;
+            string ComPortName = null;
+
+            ArrayComPortsNames = SerialPort.GetPortNames();
+            if (ArrayComPortsNames.Length > 0)
+            {
+                do
+                {
+                    index += 1;
+                    comboBox1.Items.Add(ArrayComPortsNames[index]);
+                }
+                while (!((ArrayComPortsNames[index] == ComPortName) || (index == ArrayComPortsNames.GetUpperBound(0))));
+                Array.Sort(ArrayComPortsNames);
+            }
+        }
+    }
+}
